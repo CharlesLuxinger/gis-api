07c31e3198ff63665020f2082d6cd48b60ec987f
Charles Luxinger <charlesluxinger@hotmail.com>
2020-12-07 19:05:03 -0300

    Starting repository

---
+++ src/test/java/com/github/charlesluxinger/gisapi/GisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.charlesluxinger.gisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
